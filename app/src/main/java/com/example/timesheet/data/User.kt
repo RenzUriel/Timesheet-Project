a800289a08fc8132e8f0d9aeb0fa62e813ef9c3e
roanmilestanduyan <roanmilestanduyan@gmail.com>
2025-03-25 09:40:28 +0800

    03/25/2025

--- app/src/main/java/com/example/timesheet/data/User.kt
+++ app/src/main/java/com/example/timesheet/data/User.kt
@@ -4,15 +4,15 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-data class UserResponse(
-    val token: String,
-    val user_id: String,
-    val expires: Long
-)
-
 data class User(
-    val status: String,
     val response: UserResponse
 ) {
     val token: String get() = response.token
 }
+
+data class UserResponse(
+    val token: String,
+    val user_id: String,
+    val expires: Int
+)
+
